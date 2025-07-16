8a806a51d3c5915f8b348b96c1da846403e9a114
Forest LeBlanc <fbhleblanc@gmail.com>
2025-07-16 18:33:45 -0400

    update readme and embed in widget

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# proteomics widget
+Just a small little experiment with Streamlit and some bioinformatics tools in Python.
+
+Proteomics is the analysis of proteins to understand their function. This is important because proteins interact together to form biological 'machines' in living things that can carry out extremely complex actions.
+
+This widget has 2 closely related tools. 
+
+The first processes a FASTA sequence ([Wikipedia](https://en.wikipedia.org/wiki/FASTA_format)) which describes the amino acid sequence of the protein. It provides some analysis based on this file.
+
+The second processes a PDB file ([Wikipedia](https://en.wikipedia.org/wiki/Protein_Data_Bank_(file_format))) representing the protein's physical structure. It provides a 3D graph of the protein.
+
+#### Feature plans/ideas for future
+-	More background info, help tips, etc.
+-   More customizability in output (e.g. only show aggregate summary for multi-record FASTA)
+-	More detailed FASTA sequence analysis (e.g. AA consensus or other aggregated data)
+-	Selectable graph style for PDB (e.g. highlighting alpha helices and beta sheets, or different styles such as ribbon diagrams)
+-	Graph annotation for PDB
+-	Ability to reference PDB and FASTA from online databases by ID rather than inputting files
+-	Given both PDB (physical structure) and FASTA (AA sequence) for the same protein, at least some rudimentary function prediction
+
+
+**Concepts:** *Bioinformatics, Python, Data science, proteomics*
+
+**Tools:** *Streamlit, BioPython, Pandas, plotly, numpy, py3Dmol*
--- app.py
+++ app.py
@@ -11,45 +11,51 @@ from utils.pdb import PDB
 st.title("Basic proteomics widget")
 
 #option = st.sidebar.selectbox("Choose analysis type", ["Protein sequence proteomics (FASTA)", "Protein Visualization (PDB)"])
-st.markdown(
-    "Just a small little experiment with Streamlit and some bioinformatics tools in Python."
-)
+with open('./README.md','r') as f:
+    readme_content = f.read()
 
+with st.expander("About", expanded=True):
+    st.markdown(readme_content)
+st.markdown('''
+    #### By :green[Forest LeBlanc]  
+    [forestleblanc.com](https://forestleblanc.com/)
+    ''')
 
-with st.expander("Protein sequence proteomics (FASTA)"):
+st.subheader("Protein sequence proteomics (FASTA)")
+#with st.expander("Protein sequence proteomics (FASTA)"):
 #if option == "Protein sequence proteomics (FASTA)":
     # Upload FASTA
     
-    fasta_file_input = st.file_uploader("Upload a protein FASTA file", type=["fasta", "fa"])
+fasta_file_input = st.file_uploader("Upload a protein FASTA file", type=["fasta", "fa"])
 
-    if fasta_file_input is not None:
-        fasta_input = fasta_file_input
-        st.subheader("Sequence Information")
-        fasta_file = fasta_file(file=fasta_input)
+if fasta_file_input is not None:
+    fasta_input = fasta_file_input
+    st.subheader("Sequence Information")
+    fasta_file = fasta_file(file=fasta_input)
 
-    else:
-        fasta_input = "./examples/crab_anapl.fasta"
-        st.subheader("Sequence Information (example file)")
-        fasta_file = fasta_file(file_handler=fasta_input)
+else:
+    fasta_input = "./examples/crab_anapl.fasta"
+    st.subheader("Sequence Information (example file)")
+    fasta_file = fasta_file(file_handler=fasta_input)
 
-    fasta_file.format_summary(summary_only=False, exclude=[])
+fasta_file.format_summary(summary_only=False, exclude=[])
 
 
-
-with st.expander("Protein Visualization (PDB)"):
+st.subheader("Protein Visualization (PDB)")
+#with st.expander("Protein Visualization (PDB)"):
 #elif option == "Protein Visualization (PDB)":
 
-    pdb_file = st.file_uploader("Upload a protein PDB file", type=["pdb","ent"])
+pdb_file = st.file_uploader("Upload a protein PDB file", type=["pdb","ent"])
 
-    if pdb_file is not None:
-        # Structure viewer
-        st.subheader("Protein Structure Viewer")
-        pdb = PDB(file=pdb_file) 
-        
-    else:
-        st.subheader("Protein Structure Viewer (example PDB file)")
-        pdb_filepath = "./examples/pdb1aoi.ent"
-        pdb = PDB(filepath=pdb_filepath)   
+if pdb_file is not None:
+    # Structure viewer
+    st.subheader("Protein Structure Viewer")
+    pdb = PDB(file=pdb_file) 
+    
+else:
+    st.subheader("Protein Structure Viewer (example PDB file)")
+    pdb_filepath = "./examples/pdb1aoi.ent"
+    pdb = PDB(filepath=pdb_filepath)   
 
-    st.write(pdb.format())
-    pdb.show_structure_file()
+st.write(pdb.format())
+pdb.show_structure_file()
